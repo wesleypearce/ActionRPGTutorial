8163d5dbd2b2ec45046efd75c0234b09416ba80b
Wes Pearce <wespearce1@gmail.com>
2018-09-05 00:53:38 -0500

    Player collision, Player House

---
+++ Assets/Art/roguelikeIndoor_transparent.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="roguelikeIndoor_transparent" tilewidth="16" tileheight="16" spacing="1" tilecount="468" columns="26">
+ <image source="roguelike-indoor-pack/Spritesheet/roguelikeIndoor_transparent.png" width="457" height="305"/>
+ <tile id="54">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.0434783" width="15.9565" height="15.7826"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index">
+   <object id="1" x="0.130435" y="0.0869565" width="15.8696" height="15.9565"/>
+  </objectgroup>
+ </tile>
+ <tile id="82">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0434783" y="0.0434783" width="16.087" height="15.7826"/>
+  </objectgroup>
+ </tile>
+ <tile id="184">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0434783" y="0" width="16.087" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="191">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0434783" y="0.0434783" width="15.913" height="15.913"/>
+  </objectgroup>
+ </tile>
+ <tile id="320">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0869565" y="0.0434783" width="15.7826" height="15.9565"/>
+  </objectgroup>
+ </tile>
+ <tile id="346">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="372">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0434783" y="0.0869565" width="15.9565" height="15.8696"/>
+  </objectgroup>
+ </tile>
+ <tile id="396">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0434783" y="0" width="16" height="15.913"/>
+  </objectgroup>
+ </tile>
+ <tile id="430">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.0434783" y="-0.0434783" width="15.9565" height="16.087"/>
+  </objectgroup>
+ </tile>
+ <tile id="448">
+  <objectgroup draworder="index">
+   <object id="1" x="0.0869565" y="0.0434783" width="15.8696" height="15.913"/>
+  </objectgroup>
+ </tile>
+ <tile id="456">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.130435" width="16.087" height="16.2174"/>
+  </objectgroup>
+ </tile>
+</tileset>
